0d1ad304f15e97c3242ef8268e65fe88de7a8198
Lars Haugseth <lars@larshaugseth.com>
2013-04-07 02:46:17 +0200

    Missed one file.

--- lib/topic_query.rb
+++ lib/topic_query.rb
@@ -209,8 +209,6 @@ class TopicQuery
 
       if @user_id.present?
         result = result.joins("LEFT OUTER JOIN topic_users AS tu ON (topics.id = tu.topic_id AND tu.user_id = #{@user_id})")
-      else
-        result = result.where('1 = 0') if SiteSetting.topics_are_private
       end
 
       unless query_opts[:unordered]
